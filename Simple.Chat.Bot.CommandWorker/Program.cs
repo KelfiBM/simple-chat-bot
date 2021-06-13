5ab29d772316337ff45a1c938e2e52f348e66aa7
DESKTOP\kelfi <kelfi.batista@gmail.com>
2021-06-13 17:01:59 -0400

    Adding Simple.Chat.Bot.CommandWorker project and RabbitMQ dependencies

---
+++ Simple.Chat.Bot.CommandWorker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple.Chat.Bot.CommandWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
